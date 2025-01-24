a987e5e1c05caa2c0eb7727a42e67e8787d49d56
David <95488893+DWAcademProgramming@users.noreply.github.com>
2025-01-24 16:19:04 -0500

    NuxtUI issue is resolved

---
+++ constants.js
@@ -0,0 +1,1 @@
+export const transactionView = ['Yearly', 'Monthly', 'Weekly', 'Daily']; 
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <section class="flex items-center justify-between mb-10">
+        <h1 class="text-4xl font-extrabold">Summary</h1>
+        <div>
+            <USelectMenu :options="transactionView" v-model="viewSelected" />
+        </div>
+    </section>
+</template>
+
+<script setup>
+import { transactionView } from '~/constants'
+const viewSelected = ref(transactionView[0])
+</script>
